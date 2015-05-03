1dbf77f93529cdb1546671ae6bc4cfea7ba11488
satanas <diego@redehost.com.br>
2015-05-03 15:35:14 -0300

    deleted:    license.txt 	deleted:    license2.txt 	modified:   wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,12 +1,12 @@
 <?php
 
-define('DB_NAME', 'Wp9807');
+define('DB_NAME', 'xxxxx');
 
-define('DB_USER', 'Wp9807');
+define('DB_USER', 'xxxxx');
 
-define('DB_PASSWORD', 'eqT4?D?OL4');
+define('DB_PASSWORD', 'xxxxx');
 
-define('DB_HOST', 'mysql02.redehost.com.br');
+define('DB_HOST', 'zxxxxxx');
 
 define('DB_CHARSET', 'utf8');
 
